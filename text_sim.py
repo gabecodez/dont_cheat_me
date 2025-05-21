87b19abc93a926badb377962110e127b1f2cb1e8
chickens32 <gsully32@gmail.com>
2025-05-21 08:46:57 -0400

    updated indentation

--- text_sim.py
+++ text_sim.py
@@ -5,43 +5,43 @@ allowed_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 
 #special function designed to make all text into the same, consistent format
 def standardizetext(text, standardize_letters):
-	#converts to lowercase
-	lowercase_text = text.lower()
-	#removes punctuation
-	no_punctuation_text = lowercase_text
-	
-	if standardize_letters == 1:
-		no_punctuation_text = ""
-		for letter in lowercase_text:
-			is_allowed_letter = 0
-			for other_letters in allowed_letters:
-				if letter.lower() == other_letters.lower():
-					is_allowed_letter = 1
-			
-			if is_allowed_letter == 1:
-				no_punctuation_text += letter
-	return no_punctuation_text
+    #converts to lowercase
+    lowercase_text = text.lower()
+    #removes punctuation
+    no_punctuation_text = lowercase_text
+    
+    if standardize_letters == 1:
+        no_punctuation_text = ""
+        for letter in lowercase_text:
+            is_allowed_letter = 0
+            for other_letters in allowed_letters:
+                if letter.lower() == other_letters.lower():
+                    is_allowed_letter = 1
+            
+            if is_allowed_letter == 1:
+                no_punctuation_text += letter
+    return no_punctuation_text
 
 #function designed to receive input from the user
 def getinput(prompt, required_prompt):
-	#loop that makes sure the user submits a text
-	no_text_selected = 1
-	while no_text_selected:
-		print("")
-		print(prompt)
-		#Receives text input from the user
-		return_text = str(raw_input())
-		
-		#if text is submitted, end the loop
-		if return_text != "":
-			no_text_selected = 0
-		else:
-			if required_prompt:
-				no_text_selected = 1
-			else:
-				no_text_selected = 0
-				return_text = ""
-	return return_text
+    #loop that makes sure the user submits a text
+    no_text_selected = 1
+    while no_text_selected:
+        print("")
+        print(prompt)
+        #Receives text input from the user
+        return_text = str(raw_input())
+        
+        #if text is submitted, end the loop
+        if return_text != "":
+            no_text_selected = 0
+        else:
+            if required_prompt:
+                no_text_selected = 1
+            else:
+                no_text_selected = 0
+                return_text = ""
+    return return_text
 
 #Intro that guides the user
 print("Welcome to the Text Similarity Test! Use this program to detect if two texts share some sort of connection.")
@@ -87,9 +87,9 @@ original_text_1_word_position_dictionary = {}
 #for loop that cycles through the first text and finds its position within the text, set based on the percent of the text that it is in, and stores it as a dictionary
 i = 0
 for word in original_text_1_words:
-	percent_in_text = float(((i+1)/len(original_text_1_words))*100)
-	original_text_1_word_position_dictionary[i] = {"position_percent":percent_in_text}
-	i += 1
+    percent_in_text = float(((i+1)/len(original_text_1_words))*100)
+    original_text_1_word_position_dictionary[i] = {"position_percent":percent_in_text}
+    i += 1
 
 #declares the second text's word position dictionary
 original_text_2_word_position_dictionary = {}
@@ -97,9 +97,9 @@ original_text_2_word_position_dictionary = {}
 #for loop that cycles through the second text and finds its position within the text, set based on the percent of the text that it is in
 i = 0
 for word in original_text_2_words:
-	percent_in_text = float(((i+1)/len(original_text_2_words))*100)
-	original_text_2_word_position_dictionary[i] = {"position_percent":percent_in_text}
-	i += 1
+    percent_in_text = float(((i+1)/len(original_text_2_words))*100)
+    original_text_2_word_position_dictionary[i] = {"position_percent":percent_in_text}
+    i += 1
 
 i = 0
 i2 = 0
@@ -109,28 +109,28 @@ count_of_shared_words = 0
 count_of_close_words = 0
 for word in original_text_1_words:
 
-	i2 = 0
-	#Goes through each word in the second text per first word
-	for second_word in original_text_2_words:
-		#Finds out if the two words are the same
-		if word == second_word:
-			outlawed_word_alert = 0
-			for outlawedword in outlawed_words:
-				if word == outlawedword:
-		  			outlawed_word_alert = 1
+    i2 = 0
+    #Goes through each word in the second text per first word
+    for second_word in original_text_2_words:
+        #Finds out if the two words are the same
+        if word == second_word:
+            outlawed_word_alert = 0
+            for outlawedword in outlawed_words:
+                if word == outlawedword:
+                      outlawed_word_alert = 1
 
-			#checks if it contains any of the filtered words. Skips if so
-			if outlawed_word_alert == 0:
-				#finds the difference between the two same words based on their position in the percentage throughout the text.
-				difference = abs(original_text_1_word_position_dictionary[i]["position_percent"] - original_text_2_word_position_dictionary[i2]["position_percent"])		
-				count_of_shared_words += 1
-				
-				#if the difference is less than or equal to 10, the program assumes the words are related and adds it to a counter of the close words
-				if difference <= 10:
-	  				count_of_close_words += 1
-		i2 += 1
+            #checks if it contains any of the filtered words. Skips if so
+            if outlawed_word_alert == 0:
+                #finds the difference between the two same words based on their position in the percentage throughout the text.
+                difference = abs(original_text_1_word_position_dictionary[i]["position_percent"] - original_text_2_word_position_dictionary[i2]["position_percent"])		
+                count_of_shared_words += 1
+                
+                #if the difference is less than or equal to 10, the program assumes the words are related and adds it to a counter of the close words
+                if difference <= 10:
+                      count_of_close_words += 1
+        i2 += 1
 
-	i += 1
+    i += 1
 
 #formatting designed to improve user experience
 print("")
@@ -141,16 +141,16 @@ print("")
 
 #if statement makes sure we do not divide by zero
 if count_of_shared_words != 0:
-	#finds how many words are considered to be related by the program
-	percent_of_close_words = (float(count_of_close_words)/float(count_of_shared_words))*100
+    #finds how many words are considered to be related by the program
+    percent_of_close_words = (float(count_of_close_words)/float(count_of_shared_words))*100
 else:
-	percent_of_close_words = 0
+    percent_of_close_words = 0
 
 #prints output of the percentage
 print("About "+str(int(percent_of_close_words))+"% of shared words are within the same region of the texts (error of 5%).")
 
 #program provides simple analysis. if the percentage is greater than 30%, then the program presumes the texts are related, otherwise, not.
 if percent_of_close_words > 30:
-	print("The two texts are likely related.")
+    print("The two texts are likely related.")
 else:
-  	print("The two texts are not likely related.")
+      print("The two texts are not likely related.")
